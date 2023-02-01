b87f26a3c274199fd478a3f26e584c5560da6170
jordonl27 <123746925+jordonl27@users.noreply.github.com>
2023-02-01 11:30:23 +0000

    first commit

---
+++ array.js
@@ -0,0 +1,19 @@
+/*let Favouritefilms = [
+ "Godfather",
+ "goonies",
+ "goodFellas",
+"got",
+"short circuit"
+]
+console.log(Favouritefilms);*/
+
+/*let songs = [
+    "mama",
+    "505",
+    "browneyedgirl",
+    "riotvan", 
+     "themessage"]
+{
+ console. log(songs.pop());
+}*/
+ 
